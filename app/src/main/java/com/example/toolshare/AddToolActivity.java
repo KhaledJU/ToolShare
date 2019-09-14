2d5795c587cd1b6a6cfe821ca834a516a33499ac
Khaled Jubeh <cs.jubeh@gmail.com>
2019-09-14 15:15:02 +0300

    the first step of design finished

---
+++ app/src/main/java/com/example/toolshare/AddToolActivity.java
@@ -0,0 +1,14 @@
+package com.example.toolshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddToolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_tool);
+    }
+}
