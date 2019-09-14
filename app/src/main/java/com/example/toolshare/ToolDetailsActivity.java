6629e20c2719893d4215c110386609541129db49
Khaled Jubeh <cs.jubeh@gmail.com>
2019-09-14 13:29:30 +0300

    back up

---
+++ app/src/main/java/com/example/toolshare/ToolDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.toolshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ToolDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_details);
+    }
+}
